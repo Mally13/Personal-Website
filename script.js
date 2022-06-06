606a2956433bca0f0e107a6d2c136a84d6d19de4
Mally13 <mallymutuku13@gmail.com>
2022-06-06 09:55:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+const mycards=document.querySelector(".mycards");
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+readMoreBtn.addEventListener("click", function(){
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerHTML === "Read More") {
+    readMoreBtn.innerHTML = "Read Less";
+  } else {
+    readMoreBtn.innerHTML= "Read More";
+  }
+});
+
